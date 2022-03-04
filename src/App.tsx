1714bde32b99259011b5d25fb5448e5136a52645
Kim Ji Soo <wldnd923@naver.com>
2022-03-04 16:16:43 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import styled from "styled-components";
 import Router from "./routes/Router";
-const H1 = styled.h1`
-  color: tomato;
-  font-weight: 300;
-`;
 
 function App() {
   return <Router />;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -6,8 +6,8 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Coins />} />
-        <Route path="/:coinId" element={<Coin />} />
+        <Route path="/react-test/" element={<Coins />} />
+        <Route path="/react-test/:coinId" element={<Coin />} />
       </Routes>
     </BrowserRouter>
   );
