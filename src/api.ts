fcccc0c132e0e1d826782a69088db03074df7ba3
Kim Ji Soo <wldnd923@naver.com>
2022-03-04 15:09:05 +0900

    react project test commit

---
+++ src/api.ts
@@ -0,0 +1,7 @@
+export function fetchCoins() {
+  // const data = fetch("").then(
+  //     response => response.json()
+  // );
+  // console.log(data);
+  return null;
+}
---
+++ src/theme.ts
@@ -0,0 +1,22 @@
+import { DefaultTheme } from "styled-components";
+
+// export const whiteTheme: DefaultTheme = {
+//     bgColor: "white",
+//     textColor: "black",
+//     btnColor: "tomato",
+//   };
+
+export const theme: DefaultTheme = {
+  //   bgColor: "white",
+  //   textColor: "black",
+  //   accentColor: "tomato",
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#9c88ff",
+};
+
+// export const darkTheme: DefaultTheme = {
+//   bgColor: "black",
+//   textColor: "white",
+//   btnColor: "teal",
+// };
